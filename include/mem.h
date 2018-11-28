1c476cd77a4974d2beefd077b5578ee877508b47
Axel Isaksson <isaksson.axel@telia.com>
2018-11-28 18:49:54 +0100

    added memmove

--- include/mem.h
+++ include/mem.h
@@ -9,6 +9,7 @@
 
 void *memset(void *pointer, int c, size_t n);
 void *memcpy(void *dest, void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
 
 char *strcat(char *dest, const char *src);
 size_t strlen(const char *c);
--- src/mem.c
+++ src/mem.c
@@ -21,6 +21,24 @@ void *memcpy(void *dest, void *src, size_t n) {
 	return NULL;
 }
 
+void *memmove(void *dest, const void *src, size_t n) {
+	uint8_t *p1 = dest;
+	const uint8_t *p2 = src;
+	if(src > dest) {
+		size_t i;
+		for(i = 0; i < n; i++) {
+			p1[i] = p2[i];
+		}
+	} else {
+		while(n) {
+			n--;
+			p1[n] = p2[n];
+		}
+	}
+	
+	return dest;
+}
+
 char *strcat(char *dest, const char *src) {
 	char *ret = dest;
 	while(*dest++);
